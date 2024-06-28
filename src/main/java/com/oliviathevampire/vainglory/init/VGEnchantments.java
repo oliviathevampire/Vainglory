17ff6031abaf581bc26237df425d25345eff8e62
eliatlarge <eliatlarge@gmail.com>
2024-06-27 22:04:21 -0700

    add vanilla breach and quick charge

--- src/main/java/com/oliviathevampire/vainglory/init/VGEnchantments.java
+++ src/main/java/com/oliviathevampire/vainglory/init/VGEnchantments.java
@@ -1,9 +1,15 @@
 package com.oliviathevampire.vainglory.init;
 
 import com.oliviathevampire.vainglory.Vainglory;
+import net.fabricmc.fabric.api.item.v1.EnchantmentEvents;
+import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class VGEnchantments {
 	//Non-Treasure
@@ -18,10 +24,23 @@ public class VGEnchantments {
 	public static final ResourceKey<Enchantment> EXCAVATOR = key("excavator");
 	public static final ResourceKey<Enchantment> VAINGLORY = key("vainglory");
 
-	public static final ResourceKey<Enchantment> BREACH = key("lance_breach");
-	public static final ResourceKey<Enchantment> QUICK_CHARGE = key("lance_quick_charge");
-
 	private static ResourceKey<Enchantment> key(String string) {
 		return ResourceKey.create(Registries.ENCHANTMENT, Vainglory.id(string));
+	}
+
+	private static final Set<ResourceKey<Enchantment>> VANILLA_LANCE_ENCHANTMENTS = new HashSet<>();
+
+	static {
+		VANILLA_LANCE_ENCHANTMENTS.add(Enchantments.QUICK_CHARGE);
+		VANILLA_LANCE_ENCHANTMENTS.add(Enchantments.BREACH);
+	}
+
+	public static void init() {
+		EnchantmentEvents.ALLOW_ENCHANTING.register(((enchantment, target, ctx) -> {
+			if (target.is(VGItemTags.LANCE_ENCHANTABLE) && VANILLA_LANCE_ENCHANTMENTS.stream().anyMatch(enchantment::is)) {
+				return TriState.TRUE;
+			}
+			return TriState.DEFAULT;
+		}));
 	}
 }
--- src/main/java/com/oliviathevampire/vainglory/init/VGLoot.java
+++ src/main/java/com/oliviathevampire/vainglory/init/VGLoot.java
@@ -2,7 +2,6 @@ package com.oliviathevampire.vainglory.init;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
-import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 
 public class VGLoot {
